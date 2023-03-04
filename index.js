0526308c98d7abf52a5219653b827d416b60c3b5
Sergey Filimonov <srfilimonoff@gmail.com>
2023-03-04 21:31:19 +0300

    Training JS #14: Methods of Number object--toString() and toLocaleString()

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function whatNumberIsIt(n) {
       : n;
   return `Input number is ${numCheck}`;
 }
+
+// Training JS #14: Methods of Number object--toString() and toLocaleString()
+
+function colorOf(r, g, b) {
+  return `#${r < 16 ? '0' : ''}${r.toString(16)}${
+    g < 16 ? '0' : ''
+  }${g.toString(16)}${b < 16 ? '0' : ''}${b.toString(16)}`;
+}
