0ff73a16fc6c49902ae6561f9484e58ca7f97d71
Sergey Filimonov <srfilimonoff@gmail.com>
2023-03-06 21:22:49 +0300

    Training JS #16: Methods of String object

--- index.js
+++ index.js
@@ -36,3 +36,19 @@ function howManySmaller(arr, n) {
   }
   return count;
 }
+
+// Training JS #16: Methods of String object--slice(), substring() and substr()
+
+function cutIt(arr) {
+  let shortestLength = arr[0].length;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].length < shortestLength) {
+      shortestLength = arr[i].length;
+    }
+  }
+  let shortestStringLength = [];
+  for (let i = 0; i < arr.length; i++) {
+    shortestStringLength.push(arr[i].substring(0, shortestLength));
+  }
+  return shortestStringLength;
+}
