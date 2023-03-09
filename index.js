1e7de23121457afa45546cbff10d1f92ff1786aa
Sergey Filimonov <srfilimonoff@gmail.com>
2023-03-09 23:17:46 +0300

    Training JS #19: Methods of String object

--- index.js
+++ index.js
@@ -72,3 +72,12 @@ function splitAndMerge(str, sep) {
     .map(word => word.split('').join(sep))
     .join(' ');
 }
+
+// Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace()
+
+function alienLanguage(str) {
+  return str
+    .split(' ')
+    .map(word => word.slice(0, -1).toUpperCase() + word.slice(-1).toLowerCase())
+    .join(' ');
+}
